1a94bd9839cd4ce54b5b181b373997f9b6f56238
X1Warrior <zsto7638@gmail.com>
2017-10-24 13:03:23 -0600

    Started the process to make a sound and picture changing app.

---
+++ FirstApp/Controller/MediaPageController.swift
@@ -0,0 +1,86 @@
+//
+//  MediaPageController.swift
+//  FirstApp
+//
+//  Created by Stout, Zachary on 10/24/17.
+//  Copyright © 2017 Stout, Zachary. All rights reserved.
+//
+
+    import UIKit
+    import AVFoundation
+    
+    public class MediaPageController : UIViewController
+    {
+        @IBOutlet weak var imageFrame :UIImageView!
+        @IBOutlet weak var soundButton : UIButton!
+        @IBOutlet weak var soundSlider: UISlider!
+        
+        private lazy var color : ColorTools = ColorTools()
+        private var imageCounter : Int = 0
+        private var soundPlayer : AVAudioPlayer?
+        
+        public override func viewDidLoad() -> Void
+        {
+            view.backgroundColor = color.createRandomColor()
+            loadAudioFile()
+        }
+        
+        public override func didReceiveMemoryWarning() -> Void
+        {
+            super.didReceiveMemoryWarning()
+        }
+
+@IBAction func soundButtonClick() -> Void
+{
+    playMusicFile()
+    view.backgroundColor = color.createRandomColor()
+        }
+
+        private func playMusicFile() -> Void
+        {
+            if let isPlaying = soundPlayer?.isPlaying
+            {
+                if(isPlaying)
+                {
+                    soundPlayer?.pause()
+                }
+                else
+                {
+                soundPlayer?.play()
+                }
+            }
+//            if ((soundPlayer?.isPlaying)!)
+//            {
+//                soundPlayer?.pause()
+//            }
+//            else
+//            {
+//                soundPlayer?.play()
+//            }
+        }
+        
+private func loadAudioFile() -> Void
+{
+    if let soundURL = NSDATAAsset(name: "WickedGame_clubMix")
+    {
+       do
+       {
+        try! AVAudioSession.sharedInstance().etCategory(AVAudioSessionCategoryPlayback)
+        try! AVAudioSession.sharedInstance().setActive(true)
+        
+        try soundPlayer = AVAudioPlayer(data: soundURL.data, fileTypeHint: AVFileType.mp3.rawValue)
+        soundSlider.maximumValue = Float ((soundPlayer?.duration)!)
+        Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: (#selector(self.updateSlider)), userInfo: nil, repeats: true)
+    }
+       catch
+       {
+        print("Audio file LoadError")
+        }
+    }
+}
+        @objc private func updateSlider() -> Void
+        {
+            soundSlider.value = Float ((soundPlayer?.currentTime)!)
+        }
+}
+
